d96464dbf48689ab84029ce98bd5b34953eb961f
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-14 17:26:21 +0100

    add index file

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/Db");
+require("./models/User");
+const AuthRoutes = require("./Routes/AuthRoutes");
+const requireToken = require("./middlewares/VerifyJwt");
+
+connectDB();
+
+app.use(bodyParser.json());
+
+app.use(AuthRoutes);
+
+app.get("/", requireToken, (req, res) => {
+  console.log(req.user);
+  res.send("user_id:" + req.user);
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT: ${PORT} successfully ✨`);
+});
