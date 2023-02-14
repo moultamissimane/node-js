65fb8570182207c8aefb8ebdac6b426500c463e4
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2023-02-14 17:22:21 +0100

    add models folder

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  console.log("Just before saving before hashing", user.password);
+  if (!user.isModified("password")) {
+    return next();
+  }
+  user.password = await bcrypt.hash(user.password, 8);
+  console.log("Just before saving & after hashing", user.password);
+});
+
+const User = mongoose.model("user", userSchema);
